1a8480ac2d6510b1a27ee58b61383ef17f4f7648
IgorVidalDev <igor.vidal.00000@gmail.com>
2026-01-18 18:42:03 -0300

    exercicio com metodos floor, ceil

--- Curso Js/exercicios/exercicioComNumbereMath.js
+++ Curso Js/exercicios/exercicioComNumbereMath.js
@@ -1,8 +1,14 @@
+const {floor, ceil} = Math;
+
 const numero = Number(prompt('escolha um número: '));
 
 const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
-numeroTitulo.innerHTML = `${numero} <br />`
-texto.innerHTML += `${numero} é inteiro?  ${Number.isInteger(numero)} <br />`;
-texto.innerHTML += `é NaN? ${isNaN(numero)}`
+numeroTitulo.innerHTML = numero;
+texto.innerHTML += `<p>Raiz quadrada: ${Math.sqrt(numero)} </p>`;
+texto.innerHTML += `<p>${numero} é inteiro?  ${Number.isInteger(numero)} </p>`;
+texto.innerHTML += `<p>é NaN? ${isNaN(numero)} <br />`
+texto.innerHTML += `<p>Arredondando para baixo: ${floor(numero)} </p>`;
+texto.innerHTML += `<p>Arredondando para cima: ${ceil(numero)} </p>`;
+texto.innerHTML += `<p>Com duas casas decimais ${numero.toFixed(2)}</p>`;
